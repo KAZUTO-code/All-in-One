6ebbf05011ca94d9fa7ac0f4aea7859420995192
Alto-Keita <59636913+Alto-Keita@users.noreply.github.com>
2020-01-15 17:00:21 +0900

    Update 'clacCalorie.js' from Keita
    I changed the part which called a date.
    Function 'getData()' changes to observe.

--- www/js/home/calcCalorie.js
+++ www/js/home/calcCalorie.js
@@ -1,27 +1,30 @@
 function calc()
 {
-    var data = getData();
     var setData = getSetData();
+    var weightData = getData();
     var goalCal;
-    var todayWeight = data;
-    var goalWeight = 0;//setData["goal"];
-    var duration = 0;//setData["duration"];
-    var fat = 9000;
+    var todayWeight = weightData.data;
+    var goalWeight = setData["goal"];
+    var duration = setData["duration"] - weightData.date; //残り日数
+    var fat = 9000; // 固定値
 
-    if (goalWeight == null || duration == null)
+    if (goalWeight == null || duration == null) // データがない時
     {
         document.write("エラーが発生しています。初期化してください。")
     }
+    else if (duration <= 0) // 期間が終了した時
+    {
+        document.write("現在目標が設定されていません。");
+    }
     else
     {
-        //goalCal = ((fat * (todayWeight - goalWeight)) / (duration));
-        //goalCal = Math.round(goalCal * 10) / 10; //小数点以下切り捨て
-        goalCal = 200; // remaining calorie
-        if (goalCal < 0)
+        goalCal = ((fat * (todayWeight - goalWeight)) / (duration));
+        goalCal = Math.round(goalCal * 10) / 10; //小数点以下切り捨て
+        if (goalCal < 0) // 目標達成
         {
             document.write("Congratulations on achievement!!");
         }
-        else
+        else //消費カロリー表示
         {
             document.write(goalCal + "kcal");
         }
@@ -42,37 +45,47 @@ function calc()
 
 function getData()
 {
-
-    var dateObject = new Date();
-    var year = dateObject.getFullYear();
-    var month = dateObject.getMonth() + 1;
-    if (month < 10)
-    {
-        month = "0" + month.toString();
-    }
-    var date = dateObject.getDate();
-    if (date < 10)
-    {
-        date = "0" + date.toString();
-    }
-    var key = year + "-" + month + "-" + date;
+    var setData = getSetData();
     var data = localStorage.getItem("WeightData");
-    if (data === null)
+    if (data === null) 
     {
         return null;
-    } 
-    else 
-    {
-        data = JSON.parse(data);
-        if (data[key] === null)
-        {
-            return null;
-        }
-        else
-        {
-            return data[key];
-        }
     }
+    else
+    {
+        data = JSON.parse(data); 
+    }
+    var dateObject = new Date();
+    var start = setData["startedDay"];
+    dateObject.setFullYear(start.slice(0, 4));
+    dateObject.setMonth(start.slice(5, 7) - 1);
+    dateObject.setDate(start.slice(8, 10));
+    var limit = setData["duration"];
+    var retData = new Object();
+    for (var i = 0; i < limit;) // 期間内の最新データを取得
+    {
+        var year = dateObject.getFullYear();
+        var month = dateObject.getMonth() + 1;
+        var date = dateObject.getDate();
+        if (month < 10) 
+        {
+            month = "0" + month.toString();
+        }
+        if (date < 10) 
+        {
+            date = "0" + date.toString();
+        }
+        var key = year + "-" + month + "-" + date;
+        if (data[key] != null) 
+        {
+            retData.data = data[key];
+            retData.date = i;
+            console.log(i);
+        }
+        dateObject.setDate(dateObject.getDate() + 1);
+        ++i;
+    }
+    return retData; // data : 体重, date : 日付
 }
 
 function getSetData()
